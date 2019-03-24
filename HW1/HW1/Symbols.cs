5d872fb2d89b6852f8a0434e64e393a85d140925
arisaghafari <arisa_ghafari@outlook.com>
2019-03-24 04:46:55 +0430

    symbols

---
+++ HW1/HW1/Symbols.cs
@@ -0,0 +1,35 @@
+﻿
+enum Symbols
+{
+    S_id,
+    S_number,
+    S_if,
+    S_else,
+    S_while,
+    S_do,
+    S_for,
+    S_equal,
+    S_notEqual,
+    S_closeParantez,
+    S_assign,
+    S_Number,
+    S_dot,
+    S_plus,
+    S_mul,
+    S_devide,
+    S_sub,
+    S_colon, //:
+    S_semiColon, //;
+    S_case,
+    S_switch,
+    S_break,
+    S_comma, //,
+    S_and,
+    S_or,
+    S_return,
+    S_continue,
+    S_power,
+    S_correct_devide,
+    S_openParantez, //(
+}
+
